b795af11dbd3acc8bdbd4c70137944a53aa3dd49
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-21 20:49:28 -0300

    Recebendo e validando dados do formulário

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.iniciaChat = function(applicapplication, req, res){
+    var dadosForm = req.body;
+    console.log(dadosForm);
+
+    req.assert('apelido', 'Nome ou apelido é obrigatorio').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve conter entre 5 a 15 caracteres').len(5, 15);
+
+    var erros = req.validationErrors();
+    console.log(erros)
+    if(erros){
+        res.render("index", {validacao: erros});
+        return;
+    }
+
     res.render("chat")
 }
