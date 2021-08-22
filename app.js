ebd501246ff912a73b58e42fa8145378ebed000f
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-22 11:40:50 -0300

    Integrando o soket.io ao servidor e abrindo conexão

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var server = require('./config/server');
+var app = require('./config/server');
 
-server.listen(8090, function(){
+var server = app.listen(8090, function(){
     console.log('Servidor Online')
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectado');
+    });
 });
